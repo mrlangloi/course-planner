bdb9c0bc8eb12e37dd341f3bda7806139ff306fe
Jae Eun Kim <jaeeun.je.kim@gmail.com>
2026-02-01 06:29:34 -0800

    scrolly thing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,15 @@
 import styles from './Hero.module.css';
+import logo from '../assets/Treehouse_Degree_Planner.png';
 
 const Hero = () => {
     return (
         <section className={styles["hero-section"]}>
             <div className={styles["hero-content"]}>
-                <h1>SFU CS Degree Planner</h1>
-                <h2>Plan your Computing Science Major! </h2>
+                <img src={logo} alt="Degree Planner Logo" className={styles["hero-logo"]} />
+                <div className={styles["hero-text-group"]}>
+                    <h1>SFU CS Degree Planner</h1>
+                    <h2>Plan your Computing Science Major! </h2>
+                </div>
             </div>
         </section>
     );
