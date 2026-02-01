bc43549c074fbea2267efe8049ae6abf2a05fddc
Ricky <rcheung1997@gmail.com>
2026-02-01 02:07:55 -0800

    course-planner

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+    const [courses, setCourses] = useState([]);
+
+    return (
+        <div className="container">
+            <main className="main-content">
+                <div className="courses-section">
+                    
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default App;
